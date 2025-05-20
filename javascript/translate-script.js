872af7ab1272772754051517137637334b1346c5
SuPDuZz <supduzz@gmail.com>
2025-05-20 17:04:55 +0200

    made buttons for har to type romanized characters

--- javascript/translate-script.js
+++ javascript/translate-script.js
@@ -17,50 +17,11 @@ customCharacters.forEach(char => {
     buttonsContainer.appendChild(button);
 });
 
-
+// User-defined Unicode charakters (Private Use Area)
 const alphabetMap = {
-    '\uE000': 't',   // User-defined Unicode charakter (Private Use Area)
-    '\uE001': 'c',
-    '\uE002': 'k',
-    '\uE003': 'q',
-    '\uE004': 'q̇',
-    '\uE005': '´',
-    '\uE006': 'tr',
-    '\uE007': 's',
-    '\uE008': 'kx',
-    '\uE009': 'qχ',
-    '\uE00A': 'qħ',
-    '\uE00B': 'q̇ħ',
-    '\uE00C': 'd',
-    '\uE00D': 'z',
-    '\uE00E': 'g',
-    '\uE00F': 'f',
-    '\uE010': 'th',
-    '\uE011': 'll',
-    '\uE012': 'x',
-    '\uE013': 'χ',
-    '\uE014': 'h',
-    '\uE015': 'χħ',
-    '\uE016': 'ħ',
-    '\uE017': 'r',
-    '\uE018': 'l',
-    '\uE019': 'e',
-    '\uE01A': 'æ',
-    '\uE01B': 'y',
-    '\uE01C': 'a',
-    '\uE01D': 'o',
-    '\uE01E': 'u',
-    '\uE01F': 'i',
-    '\uE020': 'ē',
-    '\uE021': 'ā',
-    '\uE022': 'ō',
-    '\uE023': 'ū',
-    '\uE024': 'ī',
-    '\uE025': 'm',
-    '\uE026': 'n',
-    '\uE027': 'ŋ',
-    '\uE028': '^',
-    ' ': ' ',
+    '\uE000': 't', '\uE001': 'c', '\uE002': 'k', '\uE003': 'q', '\uE004': 'q̇', '\uE005': '´', '\uE006': 'tr', '\uE007': 's', '\uE008': 'kx', '\uE009': 'qχ', '\uE00A': 'qħ', '\uE00B': 'q̇ħ', '\uE00C': 'd', '\uE00D': 'z', '\uE00E': 'g', '\uE00F': 'f',
+    '\uE010': 'th', '\uE011': 'll', '\uE012': 'x', '\uE013': 'χ', '\uE014': 'h', '\uE015': 'χħ', '\uE016': 'ħ', '\uE017': 'r', '\uE018': 'l', '\uE019': 'e', '\uE01A': 'æ', '\uE01B': 'y', '\uE01C': 'a', '\uE01D': 'o', '\uE01E': 'u', '\uE01F': 'i',
+    '\uE020': 'ē', '\uE021': 'ā', '\uE022': 'ō', '\uE023': 'ū', '\uE024': 'ī', '\uE025': 'm', '\uE026': 'n', '\uE027': 'ŋ', '\uE028': '^', ' ': ' ',
 };
 
 const baseVowels = ['e', 'æ', 'y', 'a', 'o', 'u', 'i', 'ē', 'ā', 'ō', 'ū', 'ī'];
@@ -107,6 +68,22 @@ function setMode(mode) {
         romanizedSection.style.display = 'block';
     }
 }
+
+// Buttons for Roman to Draconic
+const romanCharacters = ['q̇', '`', 'χ', 'ħ', 'ŋ', 'æ', 'ē', 'ā', 'ō', 'ū', 'ī', 'é', 'ǽ', 'ý', 'á', 'ó', 'ú', 'í', 'ê', 'â', 'ô', 'û', 'î'];
+const buttonsContainer2 = document.getElementById('buttons2');
+
+romanCharacters.forEach(char => {
+    const button = document.createElement('button');
+    button.textContent = char;
+    button.className = 'char-button custom-font';
+    button.addEventListener('click', () => {
+        const textarea = document.getElementById('reverseInputText');
+        textarea.value += char;
+        textarea.focus();
+    });
+    buttonsContainer2.appendChild(button);
+});
 
 // Reverse map
 const reverseAlphabetMap = {};
